bc6f2c9e39b6bcb589f3bb11f717f7a0cfb1e1a8
Satkuru Karthigeyan <satkuru@Satkurus-MBP.broadband>
2025-02-19 00:30:22 +0000

    commit initial changes with container and container compose

---
+++ services/eureka-server/src/test/java/com/skart/shopping/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skart.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
